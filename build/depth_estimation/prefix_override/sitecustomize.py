df9a20fa8f7709e10e5f16a5d5473e6e4222c800
Lenin Chavez <102402199+Hyp3Boy@users.noreply.github.com>
2024-12-06 12:25:18 -0500

    fix: erase unused files

--- build/depth_estimation/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/opt/homebrew/Caskroom/miniforge/base/envs/ros_env':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/Users/lenin.chavez/Documents/deep/DABOT_PROJECT/install/depth_estimation'
